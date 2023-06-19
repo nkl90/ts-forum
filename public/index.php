22b90cb4705a7015dcb8a95e10481978d1f96508
kengoomood <kengoomood@gmail.com>
2023-06-19 14:32:46 +0600

    Реализован роут app.topic.create. Issue №5

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Terricon\Forum\Application\Controller\ForumController;
 use Terricon\Forum\Infrastructure\NklRouting\Router;
 use Terricon\Forum\Infrastructure\ServiceContainer;
 
@@ -11,6 +12,12 @@ $routes = require_once '../config/routes.php';
 $services = require_once '../config/services.php';
 $router = new Router($routes);
 $route = $router->getRoute($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
-$serviceContainer = new ServiceContainer($services);
-$controller = $serviceContainer->getInstanceOf($route->getController());
-$controller->{$route->getAction()}(...$route->getParameters());
+//$serviceContainer = new ServiceContainer($services);
+
+$roleHierarchy = require_once '../config/permissions.php';
+$topicRepository = new \Terricon\Forum\Infrastructure\Persistence\InMemory\InMemoryTopicRepository();
+$templating = new \Terricon\Forum\Infrastructure\Templating\TemplatingEngine();
+$security = new \Terricon\Forum\Infrastructure\Security\Security($roleHierarchy);
+$controller = new ForumController($topicRepository, $templating, $security);
+
+$controller->createTopic();
